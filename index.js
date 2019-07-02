a798eaa10e5800996eaf6d5a5a6f6457825ff253
Griffin Yourick <gryphon92@gmail.com>
2019-07-02 15:13:18 -0400

    Initial functionality

---
+++ index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const repl = require('repl');
+const semver = require('semver');
+
+const rreplStr = [
+  chalk.red('r'),
+  chalk.yellow('r'),
+  chalk.green('e'),
+  chalk.blue('p'),
+  chalk.magenta('l')
+].join('');
+
+console.log(`Welcome to ${rreplStr} v1.0.0 (Node.js ${process.version})`);
+console.log(chalk.gray('Type ".help" for more information.'));
+const replServer = repl.start({
+  replMode: process.env.NODE_REPL_MODE || repl.REPL_MODE_SLOPPY
+});
+
+const home = os.homedir();
+
+if (
+  semver.gt(process.version, '11.10.0') &&
+  process.env.NODE_REPL_HISTORY !== ''
+) {
+  replServer.setupHistory(
+    process.env.NODE_REPL_HISTORY || path.join(home, '.node_repl_history'),
+    function(err, server) {
+      if (err) throw err;
+    }
+  );
+}
+
+const nodercPath = path.join(home, '.noderc');
+if (fs.existsSync(nodercPath)) {
+  try {
+    const noderc = require(nodercPath);
+    noderc(replServer);
+  } catch (err) {
+    console.log();
+    console.error('There was an error thrown by your .noderc file:');
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+replServer.prompt();
