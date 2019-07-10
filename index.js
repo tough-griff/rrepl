d33104cf9e1e648d30e9c2832321e3ed9b13bf12
Griffin Yourick <gryphon92@gmail.com>
2019-07-10 11:57:43 -0400

    Set useGlobal to true, add linting

--- index.js
+++ index.js
@@ -7,36 +7,40 @@ const path = require('path');
 const repl = require('repl');
 const semver = require('semver');
 
-const { version } = require('./package.json');
+const { version: VERSION } = require('./package.json');
+
+const { version: NODE_VERSION } = process;
 
 const RREPL_STRING = [
   chalk.red('r'),
   chalk.yellow('r'),
   chalk.green('e'),
   chalk.blue('p'),
-  chalk.magenta('l')
+  chalk.magenta('l'),
 ].join('');
 
 const { NODE_REPL_HISTORY, NODE_REPL_MODE } = process.env;
 const { REPL_MODE_SLOPPY } = repl;
 
-console.log(`Welcome to ${RREPL_STRING} v${version} (Node.js ${process.version})`);
+console.log(`Welcome to ${RREPL_STRING} v${VERSION} (Node.js ${NODE_VERSION})`);
 console.log(chalk.gray('Type ".help" for more information.'));
 const replServer = repl.start({
-  replMode: NODE_REPL_MODE || REPL_MODE_SLOPPY
+  replMode: NODE_REPL_MODE || REPL_MODE_SLOPPY,
+  useGlobal: true,
 });
 
 const home = os.homedir();
 
-if (semver.gt(process.version, '11.10.0') && NODE_REPL_HISTORY !== '') {
+if (semver.gt(NODE_VERSION, '11.10.0') && NODE_REPL_HISTORY !== '') {
   replServer.setupHistory(
     NODE_REPL_HISTORY || path.join(home, '.node_repl_history'),
-    (err, server) => {
+    (err, _server) => {
       if (err) throw err;
-    }
+    },
   );
 }
 
+/* eslint-disable global-require, import/no-dynamic-require */
 const nodercPath = path.join(home, '.noderc');
 if (fs.existsSync(nodercPath)) {
   try {
